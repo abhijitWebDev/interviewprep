afb86902a83e51f26a32669e42eb8c983947de73
abhijitWebDev <abhijitmone2@gmail.com>
2024-06-07 08:55:03 +0530

    doubly linkedlist

---
+++ doublyLinkedList/index.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(data, next=null, prev=null) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null; // Head is the first node
+        this.tail = null; // Tail is the last node
+
+    }
+
+}
